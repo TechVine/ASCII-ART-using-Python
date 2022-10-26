0849a897eed4e2e255d598f73f033c8fda546593
rishabhyaduwanshi <caml20053@glbitm.ac.in>
2022-10-26 14:41:35 +0530

    this is the code for producing ascii art of pickachu,hactoberfest logo and more using ascii magic

--- ascii.py
+++
@@ -1,8 +0,0 @@
-import pyfiglet
-from pyfiglet import Figlet
-
-fig = Figlet(font='graffiti')
-text = input("Input")
-ascii_text = pyfiglet.figlet_format(text)
-
-print(ascii_text)
--- ascii_art.py
+++
@@ -1,4 +0,0 @@
-import ascii_magic
-
-output = ascii_magic.from_image_file("sub.png",columns=100,char="&")
-ascii_magic.to_terminal(output)
